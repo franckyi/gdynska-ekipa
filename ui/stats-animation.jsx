90b8b04176875d41c5621b6cddc7f8df3d1f8a18
francky <francky@tuta.io>
2024-09-03 12:10:27 +0200

    style stats section

--- ui/stats-animation.jsx
+++ ui/stats-animation.jsx
@@ -1,36 +1,22 @@
-'use client'
-import { useRef } from 'react';
-import gsap from 'gsap';
-import { useGSAP } from '@gsap/react';
-
-gsap.registerPlugin(useGSAP);
+import { fontSecondary } from './fonts';
 
 export default function StatsAnimation({stats}) {
-    const container = useRef();
-
-    useGSAP(
-        () => {
-            // gsap code here...
-            gsap.to('.box', { x: 360 }); // <-- automatically reverted
-        },
-        { scope: container }
-    ); // <-- scope is for selector text (optional)
 
     return (
         <div className="stats-animation">
-            <div>{stats.meta.years}</div>   
-            <div>{stats.meta.projects}</div>   
-            <div>{stats.meta.cities}</div>   
-            <div className="box"></div>
-            <div className="box"></div>
-            <div className="box"></div>
-            <div className="box"></div>
-            <div className="box"></div>
-            <div className="box"></div>
-            <div className="box"></div>
-            <div className="box"></div>
-            <div className="box"></div>
-            <div className="box"></div> 
+            <div className="flex justify-between mb-12">
+                <div className='box text-8xl text-amber-600 p-8 bg-stone-900 rounded-lg hover:bg-stone-800 transition-all duration-300'>
+                    {stats.meta.years}+<span className={`text-lg text-stone-400 ${fontSecondary.className}`}> lat doświadczeniem</span>
+                    </div>   
+                <div className='box text-8xl text-amber-600 p-8 bg-stone-900 rounded-lg hover:bg-stone-800 transition-all duration-300'>
+                    {stats.meta.projects}+<span className={`text-lg text-stone-400 ${fontSecondary.className}`}> wykonywanych projektów</span>
+                    </div>
+                <div className='box text-8xl text-amber-600 p-8 bg-stone-900 rounded-lg hover:bg-stone-800 transition-all duration-300'>
+                    <span className={`text-lg text-stone-400 ${fontSecondary.className}`}>działaliśmy w </span>{stats.meta.cities}<span className={`text-lg text-stone-400 ${fontSecondary.className}`}>miast</span>
+                    </div>
+            </div>
+            <div className={`mt-32 mb-4 text-6xl text-amber-600 ${fontSecondary.className}`}>Wykonaliśmy projekty na całej Polsce</div>
+            <div className='box text-4xl'>{stats.meta.city_list}</div>
         </div>
     )
 }
--- ui/stats.jsx
+++ ui/stats.jsx
@@ -3,11 +3,10 @@ import StatsAnimation from "./stats-animation"
 export default async function Stats() {
     const data = await fetch('https://panel.gdynskaekipa.pl/wp-json/wp/v2/stats')
     let items = await data.json()
-    items = items.filter(item => item.slug == "glowne")
+    items = items.filter(item => item.slug == "glowne")[0]
 
     return(
         <div className="w-4/6 mx-auto my-32">
-            <p>show stats</p>
             <StatsAnimation stats={items} />
         </div>
     )
