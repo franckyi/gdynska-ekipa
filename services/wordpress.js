a0bb438749388b47ee3aedd380b984a9ebec6146
francky <francky@tuta.io>
2024-09-10 16:07:01 +0200

    fix typo

--- services/wordpress.js
+++ services/wordpress.js
@@ -1,12 +1,14 @@
+const duration = 3600 * 24
+
 export const getData = async (url, slug, filtered = true, ordered = false) => {
-    const response = await fetch(url);
+    const response = await fetch(url, { next: { revalidate: duration } });
     let data = await response.json();
 
     if (filtered || slug !== "") {
         data = data.filter(item => item.slug == slug)[0];
     }
 
-    if (ordered & typeof data === "array") {
+    if (ordered && typeof data === "array") {
         data = data.sort((a, b) => a.meta.display_order - b.meta.display_order)
     }
 
