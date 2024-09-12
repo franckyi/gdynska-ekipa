837bdad97b6f05d3f747a4e2653c10525f0d7eec
francky <francky@tuta.io>
2024-09-12 22:45:27 +0200

    fix our services

--- app/o-nas/page.jsx
+++ app/o-nas/page.jsx
@@ -1,8 +1,8 @@
+import Image from 'next/image'
+import parse from 'html-react-parser'
 import { getData } from '@/services/wordpress'
 import AppBar from "@/ui/app-bar";
-import parse from 'html-react-parser'
 import { fontSecondary } from "@/ui/fonts";
-import Image from 'next/image'
 import WhyUs from "@/ui/why-us";
 import Footer from "@/ui/footer";
 
@@ -10,6 +10,7 @@ export default async function AboutUs() {
     const officeUrl = 'https://panel.gdynskaekipa.pl/wp-json/wp/v2/company_office'
     const sectionsUrl = 'https://panel.gdynskaekipa.pl/wp-json/wp/v2/inne_sekcje'
     const whyUsUrl = 'https://panel.gdynskaekipa.pl/wp-json/wp/v2/dlaczego_my'
+    const servicesUrl = 'https://panel.gdynskaekipa.pl/wp-json/wp/v2/uslugi'
 
     const sections = await getData(sectionsUrl, "", false, true)
     const aboutUs = sections.find( item => item.slug === "o-nas")
@@ -17,6 +18,8 @@ export default async function AboutUs() {
     const ourTarget = sections.find( item => item.slug === "dla-kogo-jestemy")
     const whyUs = await getData(whyUsUrl, "", false, true )
     const office = await getData(officeUrl, 'gdynia', false, true)
+    const services = await getData(servicesUrl, "", false, true)
+    
 
     const titleClasses = 'mx-auto mb-20 text-5xl xl:text-8xl px-8 py-12 w-full bg-stone-900 text-center uppercase'
     const headingClasses = 'lg:mx-auto md:my-12 text-4xl lg:text-6xl lg:w-3/4 xl:text-center uppercase'
@@ -24,17 +27,17 @@ export default async function AboutUs() {
     return (
         <>
             <header className="p-8">
-                <AppBar />
+                <AppBar info={office} />
             </header>
             <main>
                 <h1 className={titleClasses}>O nas</h1>
                 <div className="w-4/6 mx-auto">
                     <div className="flex flex-wrap gap-16 my-12">
-                        <p className={`max-w-[480px] mx-auto text-amber-600 text-xl ${fontSecondary.className}`}>{info.meta.company_short_desc}</p>
-                        <p className="max-w-[480px] mx-auto">{info.meta.company_full_desc}</p>
+                        <p className={`max-w-[480px] mx-auto text-amber-600 text-xl ${fontSecondary.className}`}>{office.meta.company_short_desc}</p>
+                        <p className="max-w-[480px] mx-auto">{office.meta.company_full_desc}</p>
                     </div>
                     <h2 className={headingClasses}>Nasza misja</h2>
-                    <p className="max-w-[480px] mx-auto text-center">{info.meta.company_mission}</p>
+                    <p className="max-w-[480px] mx-auto text-center">{office.meta.company_mission}</p>
                     <Image className="mx-auto" src="/ill1.webp" width="500" height="433" alt="" />
                     <h2 id="uslugi" className={headingClasses}>Nasze usługi</h2>
                     <div className="flex flex-wrap bg-mall rounded-tr-full bg-fixed bg-no-repeat">
